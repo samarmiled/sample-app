b144d56994cef14c9e1d0df5a815f3ca874f4e34
samarmiled <samarmiled2@gmail.com>
2022-12-26 14:36:01 +0000

    Changed name docker container

--- sample-app.sh
+++ sample-app.sh
@@ -13,6 +13,6 @@ echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 cd tempdir
-docker build -t sampleapp.
-docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker build -t app .
+docker run -t -d -p 5050:5050 --name running app
 docker ps -a
